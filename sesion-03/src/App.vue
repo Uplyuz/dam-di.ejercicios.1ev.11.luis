b6544f1de42b25fc0db56989375d30727109adc4
Uplyuz <luigercm66@gmail.com>
2024-10-03 18:17:10 +0200

    mensaje

--- sesion-03/src/App.vue
+++ sesion-03/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
   import MensajeEstatico from './components/MensajeEstatico.vue';
+  import ListaEstatica from './components/ListaEstatica.vue';
 </script>
 
 <template>
   <p>EJ02-AP01: Aplicacion inicializada</p>
   <MensajeEstatico></MensajeEstatico>
+  <ListaEstatica></ListaEstatica>
 </template>
 
 <style >
---
+++ sesion-03/src/components/ListaEstatica.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <ul>
+        <li>Manzana</li>
+        <li>Banana</li>
+        <li>Cereza</li>
+    </ul>
+</template>
+
+<style scoped>
+
+</style>
