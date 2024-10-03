78d828c4fa9b7e828f295b4aa639128c3d877eea
Uplyuz <luigercm66@gmail.com>
2024-10-03 18:26:58 +0200

    mensaje

--- sesion-03/src/App.vue
+++ sesion-03/src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
   import MensajeEstatico from './components/MensajeEstatico.vue';
   import ListaEstatica from './components/ListaEstatica.vue';
+  import LibroInfo from './components/LibroInfo.vue';
 </script>
 
 <template>
   <p>EJ02-AP01: Aplicacion inicializada</p>
   <MensajeEstatico></MensajeEstatico>
   <ListaEstatica></ListaEstatica>
+  <LibroInfo></LibroInfo>
 </template>
 
 <style >
---
+++ sesion-03/src/components/LibroInfo.vue
@@ -0,0 +1,15 @@
+<script setup>
+    const Título ="Programar con Vue";
+    const Autor ="Autor Desconocido";
+    const Año= "2021";
+</script>
+
+<template>
+   <p>{{ Título }}</p>
+   <p>{{ Autor }}</p>
+   <p>{{ Año }}</p>
+</template>
+
+<style scoped>
+
+</style>
