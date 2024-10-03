d753bc0c764cd94e0ba85c2fce8559b057ca9765
Uplyuz <luigercm66@gmail.com>
2024-10-03 18:10:01 +0200

    mensaje

--- sesion-03/src/App.vue
+++ sesion-03/src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-
+  import MensajeEstatico from './components/MensajeEstatico.vue';
 </script>
 
 <template>
   <p>EJ02-AP01: Aplicacion inicializada</p>
+  <MensajeEstatico></MensajeEstatico>
 </template>
 
 <style >
---
+++ sesion-03/src/components/MensajeEstatico.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <p>Bienvenido al curso de Vue 3.</p>
+</template>
+
+<style scoped>
+
+</style>
