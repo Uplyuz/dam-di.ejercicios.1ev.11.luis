d4fc3e94d518690f7ea29b3285062a5899f68bb1
Uplyuz <luigercm66@gmail.com>
2024-10-09 21:47:57 +0200

    mensaje

--- sesion-04/src/components/HolaMundo.vue
+++ sesion-04/src/components/HolaMundo.vue
@@ -1,19 +1,25 @@
 <template>
     <div>
     <h1>{{ mensaje }}</h1>
+    <p>{{ mensajeAdicional }}</p>
     <button @click="cambiarMensaje">Cambiar Mensaje</button>
     </div>
-</template>
-   
-<script setup>
-   import { ref } from 'vue';
+   </template>
+   <script setup>
+   import { ref, computed } from 'vue';
    // Usamos ref para crear una variable reactiva
    const mensaje = ref("¡Hola, Mundo!");
    // Función para cambiar el mensaje de manera reactiva
    const cambiarMensaje = () => {
     mensaje.value = "¡Mensaje cambiado!";
    };
-</script>
+   // Propiedad computada que depende de 'mensaje'
+   const mensajeAdicional = computed(() => {
+    return mensaje.value === "¡Hola, Mundo!"
+    ? "El mensaje es el original"
+    : "El mensaje ha cambiado";
+   });
+   </script>
    
 <style >
 
