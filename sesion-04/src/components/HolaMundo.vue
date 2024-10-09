6949817dd074fdd27f2ac18e2361c1f093f2479b
Uplyuz <luigercm66@gmail.com>
2024-10-09 22:31:58 +0200

    mensaje

--- sesion-04/src/components/HolaMundo.vue
+++ sesion-04/src/components/HolaMundo.vue
@@ -3,20 +3,22 @@
     <h1>{{ datos.mensaje }}</h1>
     <p>Autor: {{ datos.autor }}</p>
     <p>Fecha: {{ datos.dia }}/{{ datos.mes }}/{{ datos.anio }}</p>
+    <p>Contador: {{ datos.contador }}</p>
    
     <button @click="cambiarDatos">Cambiar Datos</button>
     </div>
 </template>
 
 <script setup>
-   import { reactive } from 'vue';
+   import {ref,reactive,onBeforeMount,onMounted,onBeforeUpdate,onUpdated,onBeforeUnmount,onUnmounted } from 'vue';
    // Usamos reactive para crear un objeto reactivo con varias propiedades
    const datos = reactive({
     mensaje: "¡Hola, Mundo!",
     autor: "Anónimo",
     dia: 1,
     mes: 1,
-    anio: 2023
+    anio: 2023,
+    contador:0
    });
    // Función para cambiar los datos de manera reactiva
    const cambiarDatos = () => {
@@ -25,7 +27,46 @@
     datos.dia = 15;
     datos.mes = 12;
     datos.anio = 2024;
+    datos.contador=1;
    };
+
+   onBeforeMount(() => {
+  const now = new Date();
+  const time = now.toISOString().slice(11, 23); 
+  console.log(`Evento: onBeforeMount - Hora: ${time}`);
+});
+
+onMounted(() => {
+  const now = new Date();
+  const time = now.toISOString().slice(11, 23); 
+  console.log(`Evento: onMounted - Hora: ${time}`);
+});
+
+onBeforeUpdate(() => {
+  const now = new Date();
+  const time = now.toISOString().slice(11, 23); 
+  console.log(`Evento: onBeforeUpdate - Hora: ${time}`);
+  datos.contador++; 
+});
+
+onUpdated(() => {
+  const now = new Date();
+  const time = now.toISOString().slice(11, 23); 
+  console.log(`Evento: onUpdated - Hora: ${time}`);
+  
+});
+
+onBeforeUnmount(() => {
+  const now = new Date();
+  const time = now.toISOString().slice(11, 23);
+  console.log(`Evento: onBeforeUnmount - Hora: ${time}`);
+});
+
+onUnmounted(() => {
+  const now = new Date();
+  const time = now.toISOString().slice(11, 23); 
+  console.log(`Evento: onUnmounted - Hora: ${time}`);
+});
 </script>
    
 <style >
